16cadef42d595427c6d4fbb6eba58a3345a4aa70
Sanmoy1 <emailofsanmoyroy@gmail.com>
2026-03-11 22:45:33 +0530

    testing script

--- run_tests.py
+++ run_tests.py
@@ -4,7 +4,7 @@ from infrastructure.test_runner import TestRunner
 if __name__ == "__main__":
 
     torch.backends.cudnn.deterministic = True
-    test_suite_path = './test_options/7_1_cached_inference.yaml'
+    test_suite_path = './test_options/7_5_final_comparison.yaml'
     test_runner = TestRunner(test_suite_path)
     test_runner.run()
 
